d5507aabe862a76bcbfd7f237b7acd213add8cf9
edwardalx <eobkwaku@gmail.com>
2025-01-30 17:06:53 +0000

    week_v7

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS Books(
   author_id INT,
   price DOUBLE,
   publication_date DATE,
-  CONSTRAINT fk_customer_id FOREIGN Key (author_id) REFERENCES Authors(author_id)
+  CONSTRAINT fk_customer_id FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 CREATE TABLE IF NOT EXISTS Authors(
@@ -30,15 +30,15 @@ CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    Foreign Key (customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 CREATE TABLE IF NOT EXISTS Order_Details(
     order_detail_id INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    Foreign Key (order_id) REFERENCES Orders(order_id),
-    Foreign Key (book_id) REFERENCES Books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
 ALTER TABLE order_details
