ad01765ee98a206e8f5ca7142b1831beac5ed2ec
edwardalx <eobkwaku@gmail.com>
2025-01-30 00:08:24 +0000

    week8_v3

--- MySQLServer.py
+++ MySQLServer.py
@@ -4,7 +4,9 @@ mydb = mysql.connector.connect(
     username = "root",
     password = "Edward@alx2025"
 )
-
-mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-mydb.commit()
+try:
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    mydb.commit()
+except mysql.connector.Error:
+    print("These is an error with the connector") 
--- alx_book_store.sql
+++ alx_book_store.sql
@@ -5,7 +5,7 @@ USE alx_book_store;
 
 SELECT DATABASE();
 
-CREATE TABLE IF NOT EXISTS books(
+CREATE TABLE IF NOT EXISTS Books(
   book_id INT PRIMARY KEY,
   title VARCHAR(130),
   author_id INT,
@@ -14,30 +14,41 @@ CREATE TABLE IF NOT EXISTS books(
   CONSTRAINT fk_customer_id FOREIGN Key (author_id) REFERENCES authors(author_id)
 );
 
-CREATE TABLE IF NOT EXISTS authors(
+CREATE TABLE IF NOT EXISTS Authors(
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
-CREATE TABLE IF NOT EXISTS customers(
+CREATE TABLE IF NOT EXISTS Customers(
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215) UNIQUE,
     address TEXT
 ) ;
 
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     Foreign Key (customer_id) REFERENCES customers(customer_id)
 );
-CREATE TABLE IF NOT EXISTS order_details(
+CREATE TABLE IF NOT EXISTS Order_Details(
     order_detail_id INT PRIMARY KEY,
     order_id INT,
     Foreign Key (order_id) REFERENCES orders(order_id),
     book_id INT,
     Foreign Key (book_id) REFERENCES books(book_id),
     quantity DOUBLE
-);
+-- );
+-- RENAME TABLE books TO myBooks;
+-- RENAME TABLE myBooks TO Books;
+-- RENAME TABLE authors TO myAuthors;
+-- RENAME TABLE myAuthors TO Authors;
 
-#SHOW TABLES;
+-- RENAME TABLE customers TO myCustomers;
+-- RENAME TABLE myCustomers TO Customers;
+
+-- RENAME TABLE orders TO myOrders;
+-- RENAME TABLE myOrders TO Orders;
+-- RENAME TABLE order_details TO myOrder_Details;
+-- RENAME TABLE myOrder_details TO Order_Details;
+-- SHOW TABLES;
