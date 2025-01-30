dbcb88439fe4a0bf7cc8ae46c255683c44795c7f
edwardalx <eobkwaku@gmail.com>
2025-01-30 17:38:52 +0000

    week8_v8

---
+++ task_2.sql
@@ -0,0 +1,39 @@
+-- INSERT INTO books 
+-- book_id, title, author_id, price, publication_date
+-- VALUES 
+-- ("","","","","");
+CREATE TABLE IF NOT EXISTS Books(
+  book_id INT PRIMARY KEY,
+  title VARCHAR(130),
+  author_id INT,
+  price DOUBLE,
+  publication_date DATE,
+  CONSTRAINT fk_customer_id FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREATE TABLE IF NOT EXISTS Authors(
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE IF NOT EXISTS Customers(
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215) UNIQUE,
+    address TEXT
+) ;
+
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+CREATE TABLE IF NOT EXISTS Order_Details(
+    order_detail_id INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
